37fc56045d353fb7785e03726b219d5708642a57
minminhzhang <minminhzhang@unomaha.edu>
2020-02-02 16:15:56 -0600

    delete

--- javassist-project-Minmin-0128/srcfiles/Circle.java
+++
@@ -1,6 +0,0 @@
-package target;
-
-public class Circle {
-	   public int add() { return 1; }
-	   public int remove() { return 2; }
-}
--- javassist-project-Minmin-0128/srcfiles/Rectangle.java
+++
@@ -1,6 +0,0 @@
-package target;
-
-public class Rectangle {
-   public int add() { return 1; }
-   public int remove() { return 2; }
-}
